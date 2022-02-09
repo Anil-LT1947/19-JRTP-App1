100f9c0610914b78b61d45d8f90e4eb3f0ae15d9
Anil <anilkumardev1947@gmail.com>
2022-02-09 23:59:03 +0530

    my first commit

---
+++ src/main/java/in/Ashokit/_Maven_Project/App.java
@@ -0,0 +1,13 @@
+package in.Ashokit._Maven_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
